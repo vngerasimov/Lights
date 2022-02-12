49665cd552913619d1c0c23cf205f6d72c08abd3
Victor Gerasimov <gerasimo.v@mail.ru>
2022-02-12 20:47:14 +0300

    Added: the functionality of switching the screen color in the "traffic lights" format; code to turn on the flashlight on the device; application icon

--- Lights/ViewController.swift
+++ Lights/ViewController.swift
@@ -6,30 +6,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    var isLigthtOn = true
+    
+    var toggleFlash = 1
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
     // MARK: - Methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
-
+    
     fileprivate func updateUI() {
-        view.backgroundColor = isLigthtOn ? .white : .black
+        view.backgroundColor = toggleFlash == 1 ? .systemGreen : toggleFlash == 2 ? .systemRed : .systemYellow
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else {
+            return
+        }
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                device.torchMode = toggleFlash >= 2 ? .on : .off
+                device.unlockForConfiguration()
+            } catch {}
+        }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>,
                                with event: UIEvent?) {
-        isLigthtOn.toggle()
+        switch toggleFlash {
+        case 1: toggleFlash -= 1
+        case 2: toggleFlash += 1
+        default: toggleFlash = toggleFlash == 0 ? 2 : 1
+        }
         updateUI()
     }
 }
-
